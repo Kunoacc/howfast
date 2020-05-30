23d61fe72954b434f2508dbbf16596af636d98e6
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2020-05-30 03:30:57 +0100

    Changes

--- pages/home.js
+++ pages/home.js
@@ -2,8 +2,10 @@ import Head from 'next/head'
 
 export default function NewHome() {
     return (
-        <div className="flex justify-center items-start gradient-dark w-screen h-screen absolute top-0 left-0">
-            <img src="/images/logo.svg" className="mt-5"></img>
+        <div className="flex bg-dark-secondary w-screen h-screen absolute top-0 left-0">
+            <div className="flex-grow gradient-dark">
+                <img src="/images/logo.svg"></img>
+            </div>
         </div>
     )
 }
